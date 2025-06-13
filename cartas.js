400434c14d412b5782d6f3012112f715ca9f3767
leosonvezzo <leonardo.sonvezzo.martins@escola.pr.gov.br>
2025-06-13 06:19:03 -0300

    Add files via upload

---
+++ cartas.js
@@ -0,0 +1,44 @@
+// 1. Período Colonial
+criaCartao(
+    'História do Brasil',
+    'Qual foi o principal produto econômico do Brasil no século XVI?',
+    'Cana-de-açúcar (Ciclo do Açúcar).'
+);
+
+// 2. Independência
+criaCartao(
+    'História do Brasil',
+    'Quem proclamou a independência do Brasil e em que ano?',
+    'Dom Pedro I, em 7 de setembro de 1822.'
+);
+
+// 3. Era Vargas
+criaCartao(
+    'História do Brasil',
+    'Qual foi o período do Estado Novo no Brasil?',
+    '1937 a 1945, sob o governo de Getúlio Vargas.'
+);
+
+// 4. Segunda Guerra
+criaCartao(
+    'História Mundial',
+    'Em que ano o Brasil declarou guerra ao Eixo na Segunda Guerra?',
+    '1942, após ataques a navios brasileiros.'
+);
+
+// 5. Revolução Francesa
+criaCartao(
+    'História Mundial',
+    'Qual evento marcou o início da Revolução Francesa em 1789?',
+    'Queda da Bastilha em 14 de julho.'
+);
+
+
+
+// 7. Descobrimento
+criaCartao(
+    'História do Brasil',
+    'Qual navegador chegou ao Brasil em 1500 e sob qual reino?',
+    'Pedro Álvares Cabral, sob o reino de Portugal.'
+);
+
